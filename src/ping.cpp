13f7b119953777588843dff804a76a646c30a9f9
josenaldeOliveira <josenalde@gmail.com>
2020-09-10 09:40:42 -0300

    Update ping.cpp

--- src/ping.cpp
+++ src/ping.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 int main() {
-    int hostA = 0b11000000101010000000000000000010;
-    int hostB = 0b11000000101010000000000000110111;
-    int hostC = 0b11000000101010000000000100110111;
-    int maskA  = 0b11111111111111111111111100000000;
+    int hostA = 0b11000000001010100000000000000010; //192.42.0.2
+    int hostB = 0b11000000101010000000000100110111; //192.168.1.55
+    int hostC = 0b11000000101010000000000100110110; //192.168.1.54
+    int maskA  = 0b11111111111111111111111100000000; //255.255.255.0
     int maskB = maskA;
     int maskC = maskA;
     int netA = hostA ^ maskA;
     int netB = hostB ^ maskB;
     int netC = hostC ^ maskC;
-    if (netA == netB) cout << "hostA e hostB na mesma rede" << endl;
-    if (netA == netC) cout << "hostA e hostC na mesma rede" << endl;
-    else cout << "hostA e hostC em redes diferentes" << endl;
+    netA = netA >> 8; //compares only 24 bits (network)
+    netB = netB >> 8;
+    netC = netC >> 8;
+    if (netA == netB) cout << "hostA and hostB at same net" << endl;
+    else cout << "hostA and hostB at different nets" << endl;
+    
+    if (netB == netC) cout << "hostB and hostC at same net" << endl;
+    else cout << "hostB and hostC at different nets" << endl;
+    
+    if (netA == netC) cout << "hostA and hostC at same net" << endl;
+    else cout << "hostA and hostC at different nets" << endl;
 
 return 0;
 }
