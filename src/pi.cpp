f2dd21b4b90147e166471810a157eb66dd908b67
josenaldeOliveira <josenalde@gmail.com>
2020-09-21 18:12:06 -0300

    Update pi.cpp

--- src/pi.cpp
+++ src/pi.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 int main() {
+  cout.precision(11);
+  cout << fixed;
   double piant = 0, pi, termo, dif;
   int i, indiceTermo;
   double soma = 0;
   indiceTermo = 1;
   for (i = 1; ; i+=2) {
-    cout << i << endl;;
+    //cout << i << endl;;
     if (indiceTermo % 2 != 0) termo = 1./i;
     else termo = -1./i;
     
@@ -17,8 +19,7 @@ int main() {
     
     pi = 4*soma;
     dif = fabs(pi - piant);
-    cout.precision(11);
-    cout << fixed << dif;
+    cout << dif << endl;
     
     indiceTermo++;
     if (dif <= 0.005) break;
