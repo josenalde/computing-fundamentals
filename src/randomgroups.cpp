e1900f0c47ae12d62710300e0d6a2a32d53bede9
josenaldeOliveira <josenalde@gmail.com>
2021-03-08 10:04:03 -0300

    Update randomgroups.cpp

--- src/randomgroups.cpp
+++ src/randomgroups.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 int main() {
-  vector<int> t = {1,2,3,4,5,6,7,8,9,10,11};
-  vector<int> g = {1,2,3,4,5,6,7,8,9,10,11};
+  vector<int> t = {1,2,3,4,5,6,7,8,9};
+  vector<int> g = {1,2,3,4,5,6,7,8,9};
   pair<int,int> c;
   unsigned num = chrono::system_clock::now().time_since_epoch().count();
   for (int i = 0; i < t.size(); i++) {
     shuffle(t.begin(), t.end(), default_random_engine(num));
-    shuffle(g.begin(), g.end(), default_random_engine(num+1)); // a different seed
+    shuffle(g.begin(), g.end(), default_random_engine(num+10)); // a different seed
   }
   for (int i = 0; i < t.size(); i++) {
     c = make_pair(g[i], t[i]); // (group, subject)
