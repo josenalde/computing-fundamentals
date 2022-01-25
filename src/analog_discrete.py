6a939d4d3e89486b421fcb429ccf0cb3b33411c6
josenaldeOliveira <josenalde@gmail.com>
2022-01-25 08:35:28 -0300

    Update analog_discrete.py

--- src/analog_discrete.py
+++ src/analog_discrete.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 17 12:24:17 2020
-
-@author: Josenalde
-"""
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,6 +5,11 @@ t1 = np.linspace(-10,10,20) #20 points in [-10,10]
 t2 = np.linspace(-10,10,100) #100 points in [-10,10]
 ft = np.sin(t1)
 ft2 = np.sin(t2)
-#plt.plot(t1,ft, t2, ft2) #see both curves
+plt.figure(figsize=(10,15))
+plt.subplot(2,1,1)
+plt.plot(t1,ft, 'b', t2, ft2, 'r') #see both curves
+plt.grid()
+plt.subplot(2,1,2)
 plt.stem(t1,ft) #see individual plots (points)
+plt.grid()
 plt.show()
